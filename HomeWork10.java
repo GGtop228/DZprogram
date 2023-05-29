23b69be386747155f2facfcb256ebeb56ca1c66e
GGproger <130304866+GGtop228@users.noreply.github.com>
2023-05-29 14:33:25 +0500

    HomeWork10

--- HomeWork10.java
+++ HomeWork10.java
@@ -1,31 +1,31 @@
-public class MyHashSet {
-    private int[] arr;
-    private int capacity;
+public class HashSet { 
+private int[] arr; 
+private int capacity; 
 
-    public MyHashSet() {
-        capacity = 16;
-        arr = new int[capacity];
-    }
+public HashSet() { 
+capacity = 16; 
+arr = new int[capacity]; 
+} 
 
-    private int hash(int key) {
-        return key % capacity;
-    }
+private int hash(int key) { 
+return key % capacity; 
+} 
 
-    public void add(int key) {
-        int index = hash(key);
-        while(arr[index] != 0) {
-            index++;
-            index %= capacity;
-        }
-        arr[index] = key;
-    }
+public void add(int key) { 
+int index = hash(key); 
+while(arr[index] != 0) { 
+index++; 
+index %= capacity; 
+} 
+arr[index] = key; 
+} 
 
-    public void remove(int key) {
-        int index = hash(key);
-        while(arr[index] != key) {
-            index++;
-            index %= capacity;
-        }
-        arr[index] = 0;
-    }
+public void remove(int key) { 
+int index = hash(key); 
+while(arr[index] != key) { 
+index++; 
+index %= capacity; 
+} 
+arr[index] = 0; 
+} 
 }
